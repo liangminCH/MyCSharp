16b157038e2fb68e9035c930d2d6e553f298a32a
11978233@qq.com <liangMin.3658>
2021-09-11 21:05:58 +0800

    MyString\Program.cs MyString\obj\Debug\net5.0\MyString.assets.cache MyString\obj\MyString.csproj.nuget.dgspec.json MyString\obj\project.assets.json MyString\obj\project.nuget.cache

--- MyString/Program.cs
+++ MyString/Program.cs
@@ -6,7 +6,34 @@ namespace MyString
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("===================");
+
+
+
+
+            // string[][] 是一维数组，数组中的元素是string[]，相当于锯齿数组
+            string[][] arrar1 = new string[][] {
+                 new string[]{"1", "1", "1" },
+                 new string[]{"2", "2", "2" },
+                 new string[]{"3", "3", "3" }};
+
+            string[][] arrar2 = new string[][] {
+
+                 new string[]{"1", "1", "1" },
+                 new string[]{"2" },
+                 new string[]{"3", "3", "3" }};
+
+            string[][] arrar3 = new string[3][];
+
+            string[][] arrar4 = new string[4][];
+
+            //string[,] 是一个多维数组，数组每一维元素的个数必须相等
+            string[,] arr1 = new string[,] {
+                            { "1", "1", "1" },
+                            { "2", "2", "2" },
+                            {"3", "3", "3" }};
+
+            Console.WriteLine("===================");
         }
     }
 }
